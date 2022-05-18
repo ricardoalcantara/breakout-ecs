f840e1cdd9016985822b1d530b0f672aa7f89cde
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2022-05-17 23:54:42 -0300

    finally it works!

--- examples/main.rs
+++ examples/main.rs
@@ -1,20 +1,39 @@
 #![allow(unused, dead_code)]
+use std::{
+    any::{Any, TypeId},
+    collections::HashMap,
+};
+
 use breakout_ecs::world::World;
 
 fn main() {
     let mut world = World::new();
 
-    let a = world.spawn((123, true, "abc"));
     let b = world.spawn((42, false));
+    let a = world.spawn((123, true, "abc"));
 
-    for (id, (number, flag)) in world.query::<(&i32, &bool)>() {
-        println!("entity: {id} - {number} - {flag}")
+    for (id, flag) in world.query::<(&str,)>() {
+        println!("entity: {id} {}", flag.borrow())
+    }
+
+    for (id, (number, flag)) in world.query::<(i32, bool)>() {
+        println!("entity: {id} - {} - {}", number.borrow(), flag.borrow())
     }
 
     // for (id, (number, &flag)) in world.query_mut::<(&mut i32, &bool)>() {
     //     if flag {
     //         *number *= 2;
     //     }
+    // }
+
+    // let number = world.borrow_component_vec::<i32>().unwrap();
+    // let flag = world.borrow_component_vec::<bool>().unwrap();
+    // let zip = number.iter().zip(flag.iter());
+    // let iter =
+    //     zip.filter_map(|(number, flag)| Some((number.as_ref()?.borrow(), flag.as_ref()?.borrow())));
+
+    // for (number, flag) in iter {
+    //     println!("entity: _ - {number} - {flag}")
     // }
 
     // assert_eq!(*world.get::<i32>(a).unwrap(), 246);
--- src/query.rs
+++ src/query.rs
@@ -1,51 +1,74 @@
-pub trait QueryParameters {
-    type ItemFetch;
-    fn fetch(world: &mut World) -> Self::ItemFetch;
+use crate::{component::ComponentVec, world::World};
+use std::{
+    any::Any,
+    cell::{RefCell, RefMut},
+    rc::Rc,
+};
+
+pub trait Query {
+    type Item;
+    fn fetch(world: &mut World) -> QueryResult<Self::Item>;
 }
-impl<A: 'static> QueryParameters for (A,) {
-    type ItemFetch = Vec<(usize, (&'a A,))>;
-    fn fetch(world: &mut World) -> Self::ItemFetch {
-        let data = world.borrow_component_vec::<A>().unwrap();
-        let result = data
+
+pub struct QueryResult<T> {
+    pub result: Vec<T>,
+}
+
+impl<T> IntoIterator for QueryResult<T> {
+    type Item = T;
+    type IntoIter = QueryResultIntoIterator<T>;
+    fn into_iter(self) -> Self::IntoIter {
+        QueryResultIntoIterator {
+            result: self.result,
+            index: 0,
+        }
+    }
+}
+
+pub struct QueryResultIntoIterator<T> {
+    result: Vec<T>,
+    index: usize,
+}
+
+impl<T> Iterator for QueryResultIntoIterator<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.result.len() > 0 {
+            Some(self.result.remove(0))
+        } else {
+            None
+        }
+    }
+}
+
+impl<A: 'static> Query for (A,) {
+    type Item = (usize, Rc<RefCell<A>>);
+    fn fetch(world: &mut World) -> QueryResult<Self::Item> {
+        let a: Vec<(usize, Rc<RefCell<A>>)> = world
+            .borrow_component_vec::<A>()
+            .unwrap()
             .iter()
             .enumerate()
-            .filter_map(|(i, f)| Some((i, (f.as_ref()?,))))
+            .filter_map(|(i, f)| Some((i, f.as_ref()?.clone())))
             .collect();
 
-        result
-    }
-}
-impl<A: 'static, B: 'static> QueryParameters for (A, B) {
-    type ItemFetch = Vec<(usize, (A,))>;
-    fn fetch(world: &mut World) -> Self::ItemFetch {
-        todo!()
-    }
-}
-impl<A: 'static, B: 'static, C: 'static> QueryParameters for (A, B, C) {
-    type ItemFetch = Vec<(usize, (A,))>;
-    fn fetch(world: &mut World) -> Self::ItemFetch {
-        todo!()
+        QueryResult { result: a }
     }
 }
 
-pub struct QueryIterator<Q: QueryParameters> {
-    x: Q,
-}
+impl<A: 'static, B: 'static> Query for (A, B) {
+    type Item = (usize, (Rc<RefCell<A>>, Rc<RefCell<B>>));
+    fn fetch(world: &mut World) -> QueryResult<Self::Item> {
+        let a = world.borrow_component_vec::<A>().unwrap();
+        let b = world.borrow_component_vec::<B>().unwrap();
 
-// impl<Q> QueryIterator<Q> {
+        let r = a
+            .iter()
+            .zip(b.iter())
+            .enumerate()
+            .filter_map(|(i, (f1, f2))| Some((i, (f1.as_ref()?.clone(), f2.as_ref()?.clone()))))
+            .collect();
 
-//     pub fn iter(&mut self) -> QueryIter<'_, Q> {
-//         self.borrow();
-//         unsafe { QueryIter::new(self.meta, self.archetypes.iter()) }
-//     }
-// }
-
-use super::world::World;
-
-impl<Q: QueryParameters> Iterator for QueryIterator<Q> {
-    type Item = (u32, Q);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        todo!();
+        QueryResult { result: r }
     }
 }
